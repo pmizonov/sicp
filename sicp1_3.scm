ee179799196b936b70f85cf0d8005d5c25ca8c81
pmizonov <mizonovpavel@gmail.com>
2013-03-05 13:59:33 +0200

    Fixes to sicp 1_1 1_2 1_3

--- sicp1_3.scm
+++ sicp1_3.scm
@@ -1,15 +1,20 @@
 (define (square x) (* x x))
 
+(define (max_num x y) 
+  (if (> x y) x y))
+
+(define (min_num x y) 
+  (if (< x y) x y))
+
 (define (square-plus-max x y z)
-  (if (= y (max y z))
+  (if (= y (max_num y z))
       (+ (square x) (square y))
-      (+ (square x) (square z))
-  )
-)
+      (+ (square x) (square z))))
 
 (define (two-max-squares x y z)
-  (square-plus-max (max x y) (min x y) z)
-)
+  (square-plus-max (max_num x y) (min_num x y) z))
+
+;;tests
 
 (two-max-squares 3 5 4)
 (two-max-squares 3 4 5)
