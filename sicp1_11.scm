6098eb6515741497828605be19a68debb3d57cb2
pmizonov <mizonovpavel@gmail.com>
2013-03-20 15:43:28 +0200

    Exercise 1_11

---
+++ sicp1_11.scm
@@ -0,0 +1,38 @@
+#lang scheme
+;f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n > 3. 
+(define (f-recursive n)
+  (if (< n 3) 
+      n
+      (+ (f-recursive (- n 1)) (* 2 (f-recursive (- n 2))) (* 3 (f-recursive (- n 3))))))
+
+(define (f-iterative n)  
+  (if (< n 3)
+      n
+      (f-iter 0 -1 -2 0 n)))
+
+(define (f-iter n1 n2 n3 count n)
+  (if (= (+ count 1) n)
+      (f n1 n2 n3)
+      (f-iter (f n1 n2 n3) n1 n2 (+ count 1) n)
+      ))
+
+(define (f n1 n2 n3)
+  (if (< n1 2) 
+      (+ 1 n1)
+      (+ n1 (* 2 n2) (* 3 n3))))
+
+; tests
+(time (f-recursive 25))
+(time (f-iterative 25))
+
+(f-iterative 2)
+(= (f-recursive 2) (f-iterative 2))
+
+(f-iterative 3)
+(= (f-recursive 3) (f-iterative 3))
+
+(f-iterative 4)
+(= (f-recursive 4) (f-iterative 4))
+
+(f-iterative 5)
+(= (f-recursive 5) (f-iterative 5))
