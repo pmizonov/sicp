1f79b7c1f2fe9b2aaf93a205a09d73e86a0b93f4
pmizonov <mizonovpavel@gmail.com>
2013-03-21 18:02:43 +0200

    Exercise 1.14 build tree beginning

--- sicp1_14.scm
+++ sicp1_14.scm
@@ -10,6 +10,7 @@
                  (cc (- amount
                         (first-denomination kinds-of-coins))
                      kinds-of-coins)))))
+
 (define (first-denomination kinds-of-coins)
   (cond ((= kinds-of-coins 1) 1)
         ((= kinds-of-coins 2) 5)
@@ -18,3 +19,24 @@
         ((= kinds-of-coins 5) 50)))
 
 (count-change 11)
+
+;(cc 11 5)
+;(+ (cc 11 4) (cc -39 5))
+;(+ (+ (cc 11 3) (cc -14 4)) 0)
+;(+ (+ (+ (cc 11 2) (cc 1 3)) 0) 0)
+;(+ (+ (+ (+ (cc 11 1) (cc 6 2)) (+ (cc 1 2) (cc -9 3))) 0) 0)
+;(+ (+ (+ (+ (+ (cc 11 0) (cc 10 1)) (+ (cc 6 1) (cc 1 2))) (+ (+ (cc 1 1) (cc -4 2)) 0)) 0) 0)
+
+
+;(+ (+ (+ (+ (+ 0 (cc 10 1)) (+ (cc 6 1) (cc 1 2))) (+ (+ (cc 1 1) 0) 0)) 0) 0)
+
+;(cc 1 2) = (+ (cc 1 1) (cc -4 2))
+
+;(+ (cc 1 (- 2 1)) (cc (- 1 5) 2))
+;(+ (cc 1 1) (cc -4 2))
+
+;(+ (cc amount
+;       (- kinds-of-coins 1))
+;   (cc (- amount
+;          (first-denomination kinds-of-coins))
+;       kinds-of-coins))
