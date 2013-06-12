f93366117a048f92e1040bc73e53e7d5506f6235
pmizonov <pem@anahoret.com>
2013-06-12 10:43:01 +0300

    Exercise 1.16.

--- sicp1_16.scm
+++ sicp1_16.scm
@@ -1,21 +1,23 @@
 #lang scheme
-(define (expt b n)
-  (if (= n 0)
-      1
-      (* b (expt b (- n 1)))))
-
 (define (square x)
   (* x x))
+
+(define (even? n)
+  (= (remainder n 2) 0))
 
 (define (fast-expt b n)
   (cond ((= n 0) 1)
         ((even? n) (square (fast-expt b (/ n 2))))
         (else (* b (fast-expt b (- n 1))))))
 
-(define (even? n)
-  (= (remainder n 2) 0))
+(define (fast-expt-i b n)
+  (fast-expt-iter b n 1))
 
-(time (expt 2555 8500))
-;cpu time: 44 real time: 46 gc time: 16
-(time (fast-expt 2555 8500))
-;cpu time: 24 real time: 23 gc time: 24
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+        ((= n 1) (* a b))
+        ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+        (else (fast-expt-iter b (- n 1) (* a b)))))
+
+(time (fast-expt 3 6555))
+(time (fast-expt-i 3 6555))
