dee252593e4a08359baee8aade66f2ede78e102f
pmizonov <pem@anahoret.com>
2013-06-12 14:00:32 +0300

    Exercise 1.17.

---
+++ sicp1_17.scm
@@ -0,0 +1,18 @@
+#lang scheme
+(define (double x)
+  (* x 2))
+
+(define (halve x)
+  (/ x 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (*-fast a b)
+  (cond ((= b 0) 0)
+        ((= b 1) a)
+        ((even? b) (*-fast (double a) (halve b)))
+        (else (+ a (*-fast a (- b 1))))))
+
+(time (* 3 1212332132131231678678678678678546464204211))
+(time (*-fast 3 1212332132131231678678678678678546464204211))
