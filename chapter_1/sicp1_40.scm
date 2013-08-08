8998752c6f5bf050e2c7608489999881ac46534c
pmizonov <pem@anahoret.com>
2013-08-08 18:18:27 +0300

    Exercise 1.40.

---
+++ chapter_1/sicp1_40.scm
@@ -0,0 +1,46 @@
+#lang scheme
+
+(define tolerance 0.00001)
+
+(define dx 0.00001)
+
+(define (cube x)
+  (* x x x))
+
+(define (square x)
+  (* x x))
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+  (lambda (x) (+ (cube x)
+                 (* a (square x))
+                 (* b x)
+                 c)))
+
+;(newtons-method (cubic a b c) 1)
+
+(newtons-method (cubic 4 8 3) 1)
+
+((cubic 4 8 3) (newtons-method (cubic 4 8 3) 1))
