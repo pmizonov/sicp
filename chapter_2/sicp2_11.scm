b7abff2d72ff62301bc937ac7af2a4f129d23b5d
pmizonov <pem@anahoret.com>
2013-10-03 18:27:43 +0300

    Exercise 2.11.

--- chapter_2/sicp2_11.scm
+++ chapter_2/sicp2_11.scm
@@ -10,6 +10,31 @@
   (cdr interval))
 
 (define (mul-interval x y)
+  (let ((ll (* (lower-bound x) (lower-bound y)))
+        (lu (* (lower-bound x) (upper-bound y)))
+        (ul (* (upper-bound x) (lower-bound y)))
+        (uu (* (upper-bound x) (upper-bound y))))
+    
+    (cond ((and (> (lower-bound x) 0) (> (lower-bound y) 0)) 
+           (make-interval ll uu))
+          ((and (> (lower-bound x) 0) (< (lower-bound y) 0) (> (upper-bound y) 0))
+           (make-interval ul uu))
+          ((and (> (lower-bound x) 0) (< (upper-bound y) 0))
+           (make-interval ul lu))
+          ((and (< (lower-bound x) 0) (> (upper-bound x) 0) (> (lower-bound y) 0))
+           (make-interval lu uu))
+          ((and (< (lower-bound x) 0) (> (upper-bound x) 0) (< (lower-bound y) 0) (> (upper-bound y) 0))
+           (make-interval (min lu ul) (max ll uu)))
+          ((and (< (lower-bound x) 0) (> (upper-bound x) 0) (< (upper-bound y) 0))
+           (make-interval ul ll))
+          ((and (< (upper-bound x) 0) (> (lower-bound y) 0))
+           (make-interval lu ul))
+          ((and (< (upper-bound x) 0) (< (lower-bound y) 0) (> (upper-bound y) 0))
+           (make-interval lu ll))
+          ((and (< (upper-bound x) 0) (< (upper-bound y) 0))
+           (make-interval uu ll)))))
+
+(define (mul-interval-old x y)
   (let ((p1 (* (lower-bound x) (lower-bound y)))
         (p2 (* (lower-bound x) (upper-bound y)))
         (p3 (* (upper-bound x) (lower-bound y)))
@@ -18,20 +43,40 @@
                    (max p1 p2 p3 p4))))
 
 (define i++ (make-interval 1 2))
-(define i-+ (make-interval -1 2))
+(define i-+ (make-interval -4 2))
 (define i-- (make-interval -2 -1))
 
+(mul-interval i++ i++)
+(mul-interval-old i++ i++)
+(mul-interval i++ i-+)
+(mul-interval-old i++ i-+)
+(mul-interval i++ i--)
+(mul-interval-old i++ i--)
+(mul-interval i-+ i++)
+(mul-interval-old i-+ i++)
+(mul-interval i-+ i-+)
+(mul-interval-old i-+ i-+)
+(mul-interval i-+ i--)
+(mul-interval-old i-+ i--)
+(mul-interval i-- i++)
+(mul-interval-old i-- i++)
+(mul-interval i-- i-+)
+(mul-interval-old i-- i-+)
+(mul-interval i-- i--)
+(mul-interval-old i-- i--)
+
 ; При умножении выбирается минимум и максимум из произведений границ множителей.
-; Рассмотрим знаки произведений границ при различных знаках множителей, учитывая,
+; Определим максимум и минимум произведений границ при различных знаках множителей, учитывая,
 ; что у интервала верхняя граница больше нижней:
-; (+,+)*(+,+) p1 + p2 + p3 + p4 +
-; (+,+)*(-,+) p1 - p2 + p3 - p4 +
-; (+,+)*(-,-) p1 - p2 - p3 - p4 -
 
-; (-,+)*(+,+) p1 - p2 - p3 + p4 +
-; (-,+)*(-,+) p1 + p2 - p3 - p4 +
-; (-,+)*(-,-) p1 + p2 + p3 - p4 -
+; (+,+)*(+,+) ll + lu + ul + uu +  min ll max uu
+; (+,+)*(-,+) ll - lu + ul - uu +  min ul max uu
+; (+,+)*(-,-) ll - lu - ul - uu -  min ul max lu
 
-; (-,-)*(+,+) p1 - p2 - p3 - p4 -
-; (-,-)*(-,+) p1 + p2 - p3 + p4 -
-; (-,-)*(-,-) p1 + p2 + p3 + p4 +
+; (-,+)*(+,+) ll - lu - ul + uu +  min lu max uu
+; (-,+)*(-,+) ll + lu - ul - uu +  min lu ul max ll uu
+; (-,+)*(-,-) ll + lu + ul - uu -  min ul max ll
+
+; (-,-)*(+,+) ll - lu - ul - uu -  min lu max ul
+; (-,-)*(-,+) ll + lu - ul + uu -  min lu max ll
+; (-,-)*(-,-) ll + lu + ul + uu +  min uu max ll
