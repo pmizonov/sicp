60a91907b8d4f63b6d4bcd92f071f3b96613f730
pmizonov <pem@anahoret.com>
2013-12-13 17:28:17 +0200

    Exercise 2.38 and 2.39.

---
+++ chapter_2/sicp2_38.scm
@@ -0,0 +1,27 @@
+#lang scheme
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(fold-right list null (list 1 2 3))
+(fold-left list null (list 1 2 3))
+
+; 3/2
+; 1/6
+; (1 (2 (3 ())))
+; (((() 1) 2) 3)
+
+; Чтобы результат был одинаковый операция должна обладать свойством коммутативности.
